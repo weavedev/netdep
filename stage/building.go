73ecb90ea8d2017b740cee3bf3812ada4ebc8f7d
Gints Kuļikovskis <gintskk@gmail.com>
2022-04-29 16:02:54 +0200

    Add template files and descriptions
    Descriptions of different stages of the program

---
+++ stage/building.go
@@ -0,0 +1,10 @@
+package stage
+
+/*
+Copyright © 2022 Team 1, Weave BV, TU Delft
+
+In the Building stage, the adjacency lists of each service are populated.
+This is done by traversing the lists of endpoints/clients and looking for the other end of the connection.
+The Building stage should handle
+Refer to the Project plan, chapter 5.4 for more information.
+*/
