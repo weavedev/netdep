e282b0f2956349c2d8863220037acc6a013a5e57
Lukas Ostrovskis <lukas.ostrovskis1@gmail.com>
2022-04-29 15:15:03 +0200

    chore: added depScan command for dependency scan

--- cmd/root.go
+++ cmd/root.go
@@ -10,8 +10,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "static-analysis-project",
@@ -42,10 +40,7 @@ func init() {
 	// will be global for your application.
 
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.static-analysis-project.yaml)")
-
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
