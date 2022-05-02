6b745fc9f6fcf92e9b22479ce9d04a4b2565c0aa
Delano Flipse <delanoflipse@hotmail.com>
2022-05-02 12:55:14 +0200

    Extend example, add markers

--- test/example/svc/node-two/main.go
+++ test/example/svc/node-two/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/hashicorp/go-retryablehttp"
+)
+
 func main() {
-	// ...
+	targetUrl := "http://httpbin.org/get"
+	// resty client get request
+	client := resty.New()
+	resp, err := client.R().Get(targetUrl)
+
+	// retryablehttp
+	retryClient := retryablehttp.NewClient()
+	req, err := http.NewRequestWithContext(context.Background(), "GET", targetUrl, nil)
+	retryClient.Client.Do(req)
 }
