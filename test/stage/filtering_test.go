a46992a78dbdec5638b0f97706071d6c22a626dc
Gints Kuļikovskis <gintskk@gmail.com>
2022-05-02 16:31:43 +0200

    chore: fix TestFiltering sample test

--- test/stage/filtering_test.go
+++ test/stage/filtering_test.go
@@ -2,6 +2,7 @@ package stage
 
 import (
 	"github.com/stretchr/testify/assert"
+	"go/ast"
 	"lab.weave.nl/internships/tud-2022/static-analysis-project/stage"
 	"testing"
 )
@@ -11,5 +12,5 @@ A test for the sample implementation of the resolution method
 */
 func TestFiltering(t *testing.T) {
 	var res = stage.ScanAndFilter("test")
-	assert.Equal(t, make([]string, 0), res, "Expect nil as the output of the ScanAndFilter method")
+	assert.Equal(t, map[string][]*ast.File(nil), res, "Expect nil as the output of the ScanAndFilter method")
 }
