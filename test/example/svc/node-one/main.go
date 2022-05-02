64b91ef20d1fcd66def8048793fc51f6dc13b9b8
Delano Flipse <delanoflipse@hotmail.com>
2022-05-02 12:55:14 +0200

    chore: extend example, add markers

--- test/example/svc/node-one/main.go
+++ test/example/svc/node-one/main.go
@@ -6,23 +6,19 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/go-resty/resty/v2"
-
 	httpUtils "lab.weave.nl/nid/nid-core/pkg/utilities/http"
 )
 
 // http call using http request object
-func performRequest(client httpUtils.Client, host string) {
+func performRequest(client http.Client, host string) {
+	// intialise request, based on https://github.com/nID-sourcecode/nid-core/blob/main/svc/connectinmesh/main.go
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "https://"+host+"/hello", nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
 
-		return
-	}
-
+	// for testing purposes, mutate request object
 	newUrl, err := url.Parse("https://" + host + "/hello2")
 	req.URL = newUrl
 
+	// @mark HTTP request to https://$OUTSIDE_MESH/hello2
 	resp, err := client.Do(req)
 
 	if err != nil {
@@ -33,8 +29,11 @@ func performRequest(client httpUtils.Client, host string) {
 }
 
 func main() {
-	// resty client get request
-	client := resty.New()
+	// initialise http client
+	httpClient := &http.Client{}
+	// get host from env
+	host := os.Getenv("OUTSIDE_MESH")
 
-	resp, err := client.R().Get("http://httpbin.org/get")
+	// perform request
+	performRequest(httpClient, host)
 }
