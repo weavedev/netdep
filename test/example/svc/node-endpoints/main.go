21de5703b248031114935ad4d5e313ec751144c3
Delano Flipse <delanoflipse@hotmail.com>
2022-05-03 15:26:05 +0200

    add both standard and gin based http endpoints

--- test/example/svc/node-endpoints/main.go
+++ test/example/svc/node-endpoints/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// Void endpoint
+	// @mark HTTP endpoint "/bar"
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	// Endpoint that calls endpoints
+	// @mark HTTP endpoint "/foo"
+	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		// @mark HTTP call to "http://example.com/"
+		http.Get("http://example.com/")
+
+		fmt.Fprintf(w, "Hello Foo")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
