7ad693b87084b18d691003e557f5bd963c23a0a5
Delano Flipse <delanoflipse@hotmail.com>
2022-05-04 12:25:00 +0200

    feat: add WIP code of variable resolve

--- experiment/analyze/analyze_package.go
+++ experiment/analyze/analyze_package.go
@@ -2,7 +2,20 @@ package analyze
 
 import (
 	"fmt"
+	types "go/types"
 	"golang.org/x/tools/go/ssa"
+	"strings"
+)
+
+var (
+	interestingCalls = map[string]map[string]bool{
+		"net/http": {
+			"Get":  true,
+			"Post": true,
+			"Put":  true,
+			"Do":   true,
+		},
+	}
 )
 
 func getMainFunction(pkg *ssa.Package) *ssa.Function {
@@ -18,12 +31,51 @@ func getMainFunction(pkg *ssa.Package) *ssa.Function {
 	return mainFunction
 }
 
+func resolveVariables(parameters []ssa.Value) []string {
+	stringParameters := make([]string, len(parameters))
+	for i, val := range parameters {
+		parameter, ok := val.(*ssa.Parameter)
+		if ok {
+			stringParameters[i] = resolveParameterVariable(parameter)
+		} else {
+			stringParameters[i] = "[err]"
+		}
+	}
+
+	return stringParameters
+}
+
+func resolveParameterVariable(parameter *ssa.Parameter) string {
+	parameterName := parameter.Name()
+	prog := parameter.Parent().Prog
+	pkg := parameter.Parent().Pkg
+	switch par := parameter.Object().(type) {
+	case *types.Var:
+		// TODO: find node?
+		value, isAddr := prog.VarValue(par, pkg, nil)
+		fmt.Println(parameterName, value, isAddr)
+	}
+	return "var(" + parameterName + ") = ??"
+}
+
 func discoverCall(call *ssa.Call) {
 	calledFunction, _ := call.Call.Value.(*ssa.Function)
 	calledFunctionPackage := calledFunction.Pkg.Pkg.Path()
 
 	fmt.Println("Called function " + calledFunctionPackage + "->" + calledFunction.Name())
-	// args
+
+	interestingPackage, isInterestingPackage := interestingCalls[calledFunctionPackage]
+	if isInterestingPackage {
+		_, isInterestingFunction := interestingPackage[calledFunction.Name()]
+		if isInterestingFunction {
+			fmt.Println("Found call to function " + calledFunctionPackage + "." + calledFunction.Name() + "()")
+
+			if call.Call.Args != nil {
+				arguments := resolveVariables(call.Call.Args)
+				fmt.Println("Arguments: " + strings.Join(arguments, ", "))
+			}
+		}
+	}
 
 	if calledFunction.Blocks != nil {
 		discoverBlocks(calledFunction.Blocks)
