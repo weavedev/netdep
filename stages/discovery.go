2721424e7489c9dccf7c31c591034942733b5613
Lukas Ostrovskis <lukas.ostrovskis1@gmail.com>
2022-05-09 13:04:45 +0200

    feat(outp): add initial output generation

--- stages/discovery.go
+++ stages/discovery.go
@@ -8,6 +8,20 @@ In the Discovery stages, clients and endpoints are discovered and mapped to thei
 Refer to the Project plan, chapter 5.3 for more information.
 */
 
+// ServiceCalls stores for each service its name and the calls that it makes (strings of URLs / method names)
+type ServiceCalls struct {
+	Service string
+	Calls   map[string]int
+}
+
+// DiscoveredData is initialised and populated during the discovery stage.
+// It stores a list of ServiceCalls for each service and a map of all handled endpoints / methods
+// along with the name of the service that handles each one.
+type DiscoveredData struct {
+	ServCalls []ServiceCalls
+	Handled   map[string]string
+}
+
 // FindCallersForEndpoint is a sample method for locating
 // the callers of a specific endpoint, which is specified
 // by the name of its parent service, its path in the target
