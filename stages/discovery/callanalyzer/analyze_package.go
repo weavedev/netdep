6d3adaf9f51ed8b0243aa540b9984471014ba996
Gints Kuļikovskis <gintskk@gmail.com>
2022-05-12 15:46:01 +0200

    chore: rename blacklist and signature

--- stages/discovery/callanalyzer/analyze_package.go
+++ stages/discovery/callanalyzer/analyze_package.go
@@ -11,7 +11,8 @@ import (
 	"golang.org/x/tools/go/ssa"
 )
 
-var blackList = map[string]bool{
+// ignoreList is a set of function names to not recurse into
+var ignoreList = map[string]bool{
 	"fmt":                  true,
 	"reflect":              true,
 	"net/url":              true,
@@ -24,6 +25,8 @@ var blackList = map[string]bool{
 	"internal/reflectlite": true,
 }
 
+// DiscoveryAction indicates what to do when encountering
+// a certain call. Used in interestingCalls
 type DiscoveryAction int64
 
 const (
@@ -31,10 +34,9 @@ const (
 	Substitute                 = 1
 )
 
-// interestingCalls is used Stores Relevant Libraries
-// their Relevant Methods and for each method
-// a position of location in the Args of ssa.Call
-//nolint
+// interestingCalls is a map from target to action that is to be taken when encountering the target.
+// Used internally to distinguish whether a call is to be:
+// outputted as a party in a dependency (0) or substituted with a constant (1)
 var (
 	interestingCalls = map[string]DiscoveryAction{
 		"(*net/http.Client).Do": Output,
@@ -70,40 +72,46 @@ func getPackageFunction(pkg *ssa.Package, name string) *ssa.Function {
 
 // recurseOnTheTarget recursively traverses the SSA, with call being the starting point,
 // and using the environment specified in the frame
+// Variables are only resolved if the call is 'interesting'
+// Recursion is only continued if the call is not in the 'ignoreList'
 func recurseOnTheTarget(call *ssa.Call, frame Frame) {
+	// The fnCallType can be the function type, the anonymous function type, or something else.
+	// See https://pkg.go.dev/golang.org/x/tools/go/ssa#Call
 	switch fnCallType := call.Call.Value.(type) {
 
+	// TODO: handle other kinds of call targets
 	case *ssa.Function:
-		signature := fnCallType.RelString(nil)
+		// The full qualified name of the function, including its package
+		qualifiedFunctionName := fnCallType.RelString(nil)
 		rootPackage := fnCallType.Pkg.Pkg.Path()
 
-		_, isInteresting := interestingCalls[signature]
+		_, isInteresting := interestingCalls[qualifiedFunctionName]
 		if isInteresting {
-
+			// Resolve all arguments of the function call (because it is in the interesting list)
 			arguments := resolveVariables(call.Call.Args, frame)
-			fmt.Println("Arguments: " + strings.Join(arguments, ", "))
+			//fmt.Println("Arguments: " + strings.Join(arguments, ", "))
 
-			caller := &Target{
+			callTarget := &Target{
 				requestLocation: strings.Join(arguments, "/"),
 				library:         rootPackage,
-				methodName:      signature,
+				methodName:      qualifiedFunctionName,
 			}
 
-			fmt.Println("Found call to function " + signature)
+			//fmt.Println("Found call to function " + qualifiedFunctionName)
 			//TODO Handle error
-			_ = append(*frame.targets, caller)
+			_ = append(*frame.targets, callTarget)
 			return
 		}
 
-		_, isBlacklisted := blackList[rootPackage]
+		_, isIgnored := ignoreList[rootPackage]
 
-		if isBlacklisted {
-			// Do not step into the library if it is blacklisted for recursion
+		if isIgnored {
+			// Do not recurse into the library if it is ignored (for recursion)
 			return
 		}
 
 		//frame.mappings = make(map[string]ssa.Value)
-		fmt.Println("Called function " + signature + " " + rootPackage)
+		//fmt.Println("Called function " + qualifiedFunctionName + " " + rootPackage)
 
 		for i, param := range fnCallType.Params {
 			frame.Mappings[param.Name()] = call.Call.Args[i]
