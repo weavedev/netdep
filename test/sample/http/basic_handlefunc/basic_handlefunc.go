368fdabba1576448681dec3c86192efa4539faa5
Lukas Ostrovskis <lukas.ostrovskis1@gmail.com>
2022-05-17 09:48:37 +0200

    chore: add server endpoint test samples

---
+++ test/sample/http/basic_handlefunc/basic_handlefunc.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/test", HelloHandler)
+
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HelloHandler(w http.ResponseWriter, _ *http.Request) {
+
+	fmt.Fprintf(w, "Hello, there\n")
+}
