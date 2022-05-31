bd6e341bca30e9655d41670458500c9fb0420d32
Delano Flipse <delanoflipse@hotmail.com>
2022-05-31 14:58:09 +0200

    chore: make use of slice subset for loop

--- stages/discovery/callanalyzer/trace_reporter.go
+++ stages/discovery/callanalyzer/trace_reporter.go
@@ -13,12 +13,12 @@ func PrintTraceToCall(trace []*ssa.Call, frame *Frame, config *AnalyserConfig) {
 	// as defined by config.maxTraceDepth
 	displayStartingIndex := traces - config.maxTraceDepth
 
-	for i, call := range trace {
-		// only show the last few items, as defined in the config
-		if i < displayStartingIndex {
-			continue
-		}
+	// could use max(displayStartingIndex, 0), but this is more explicit.
+	if displayStartingIndex < 0 {
+		displayStartingIndex = 0
+	}
 
+	for i, call := range trace[displayStartingIndex:] {
 		_, file, position := getCallInformation(call.Pos(), frame.pkg)
 		signature := ""
 		switch callee := call.Call.Value.(type) {
